8997ef0bc30817578ddcaceac57024cb7b6712b7
UlviSuleiman <ulvisuleiman@gmail.com>
2024-02-06 16:55:08 +0400

    Satinalma islemi onayi eklendi

--- src/Day01/Merhaba.java
+++ src/Day01/Merhaba.java
@@ -5,7 +5,10 @@ public class Merhaba {
 
         System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
-        System.out.println("Merhaba 3");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+        }
 
 
 
