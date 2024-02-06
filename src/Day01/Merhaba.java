3c591eed772886ab454c4b5971ff079d8049a363
UlviSuleiman <ulvisuleiman@gmail.com>
2024-02-06 16:57:28 +0400

    task 3

--- src/Day01/Merhaba.java
+++ src/Day01/Merhaba.java
@@ -4,7 +4,10 @@ public class Merhaba {
     public static void main(String[] args) {
 
         System.out.println("Merhaba 1");
-        System.out.println("Merhaba 2");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba 5");
+        }
 
         for (int i = 0; i < 5; i++) {
             System.out.println("merhaba");
