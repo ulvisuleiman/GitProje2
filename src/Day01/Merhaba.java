e9dba6baf3da05d3a2b979b84730d3194fe324e9
UlviSuleiman <ulvisuleiman@gmail.com>
2024-02-06 16:51:54 +0400

    Sepet urun eklenmesi ve testi eklendi

---
+++ src/Day01/Merhaba.java
@@ -0,0 +1,12 @@
+package Day01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+        System.out.println("Merhaba 2");
+
+
+
+    }
+}
